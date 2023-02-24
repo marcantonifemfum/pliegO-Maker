1b18ec4d5852d8121cc00783ce728167d2c72547
Marcantoni Malagarriga-Picas <marcantoni@femfum.com>
2023-02-24 11:06:26 +0100

    Implementació per a ús en servidors

--- pliegOmaker.php
+++ pliegOmaker.php
@@ -9,12 +9,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET')
  putenv('hv=');
  putenv('full=');
  putenv('r=');
- putenv('lab=');
+ putenv('labal=');
+ putenv('tlab=');
 
- // pliegos de 8 amb reglatge
- // http://localhost/www.pliegos.net/maker/pliegOmaker.php?vp=true&nup=8&hv=true&full=4&r=false&lab=0
- // pliegos de 8 amb l'hora
- // http://localhost/www.pliegos.net/maker/pliegOmaker.php?vp=true&nup=8&hv=true&full=4&r=false&lab=1
+ // crida sense paràmetres per l'execució única del prototip de pliegOS pel projecte sonar+D 2023
+ // http://localhost/www.pliegos.net/maker/sonarD2023/pliegOmaker.php
  $vp=$_GET['vp'];
 //echo($vp.'<br>');
  $nup=$_GET['nUp'];
@@ -25,12 +24,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET')
 //echo($full.'<br>');
  $r=$_GET['r'];
 //echo($r.'<br>');
- $lab=$_GET['lab'];
+ $labal=$_GET['labal'];
 //echo($lab.'<br>');
+ $tlab=$_GET['tlab'];
 //exit(' ...em veus?');
 }
 else
-{  // Si fem POST: les variables vindran triades des d'index.html
+{  // Si fem POST: les variables vindran triades des d'index_##.php
 
  $vp=$_POST['vp'];
  //echo $vp;  // ensenyem vores de les pàgines?
@@ -47,8 +47,12 @@ else
  $r=$_POST['r'];
  //echo $r;  // mantenim la ràtio?
 
- $lab=$_POST['lab'];
- //echo $lab;  // algorisme
+ $labal=$_POST['labal'];
+ //echo $labal;  // algorisme
+ $tlab=$_POST['tlab'];
+// echo $tlab;  // text de la caixa
+// echo $_POST['tlab'];  // text a compondre
+// exit(' ...bon dia!');
 }
 
 // minimitzem el trànsit de dades a través de variables d'entorn i així ens estalviem d'escriure les dades a disc
@@ -60,26 +64,65 @@ putenv("MRCT_nup=$nup");  // desem l'nup a la variable d'entorn
 putenv("MRCT_hv=$hv");  // desem l'orientació a la variable d'entorn 
 putenv("MRCT_full=$full");  // desem el format del full d'impressió a la variable d'entorn 
 putenv("MRCT_r=$r");  // desem si mantenim la ràtio a la variable d'entorn 
-putenv("MRCT_lab=$lab");  // desem l'algorisme de composició a la variable d'entorn 
+putenv("MRCT_labal=$labal");  // desem l'algorisme de composició a la variable d'entorn 
+putenv("MRCT_tlab=$tlab");  // desem el text a compondre a la variable d'entorn 
+
+// treballem amb l'hora del client i no pas amb la del servidor
+// cridem via PHP una galeta desada via JS a index_##.php
+// exit($_COOKIE['quinHoraEs'].' ...en podem extraure l\'hora del client?');
+// i la tornem a desar en una nova variabe d'entorn
+//URLsnr (hauríem de generar aquí la galeta horària --com fem a index_##.php-- i seguidament cridar-la com ja fem)
+$ficali = $_COOKIE['quinHoraEs'];
+putenv("MRCT_qhe=$ficali");  // el valor de la galeta que duu l'hora del client en format (hh:mm:ss)
 
 //en tenim prou com a nom únic?
 $PDFunic = date("d"."B"."H"."i"."s");
 
 //$somaPS = "/home/marcantoni/pliegos.net/maker/";  // path al ps al servidor www.pliegos.net del DreamHost
-$somaPS = "/Library/WebServer/Documents/www.pliegos.net/maker/";  // path al ps al localhost del Macbook Air
+//$somaPS = "/var/www/wordpress/maker/plegaVeu/";  // path al ps al nou servidor www.pliegos.net de Teixidora
+//$somaPS = "/Library/WebServer/Documents/www.pliegos.net/maker/plegaVeu/";  // path al ps al localhost del Macbook Air
+
+//URLsnr
+$somaPS = "/Library/WebServer/Documents/www.pliegos.net/maker/sonarD2023/";  // path al ps cap al localhost del MacbookAir
+
 
 //$somaPDF = "/home/marcantoni/pliegos.net/maker/pdf/";  // path al pdf al servidor www.pliegos.net del DreamHost
-$somaPDF = "/Library/WebServer/Documents/www.pliegos.net/maker/pdf/";  // path al pdf al localhost del Macbook Air
+//$somaPDF = "/var/www/wordpress/maker/plegaVeu/pdf/";  // path al pdf al nou servidor www.pliegos.net de Teixidora
+//$somaPDF = "/Library/WebServer/Documents/www.pliegos.net/maker/plegaVeu/pdf/";  // path al pdf al localhost del Macbook Air
 
-//$somaGS = "/usr/bin/";  // path a l'executable de Ghostscript al servidor www.pliegos.net del DreamHost
+//URLsnr
+//@EP el directori /pdf on s'allotgen els resultats ha de tenir tots els permisos! (si no peta el GS sense que ens digui perquè)
+$somaPDF = "/Library/WebServer/Documents/www.pliegos.net/maker/sonarD2023/pdf/";  // path al pdf cap al localhost del MacbookAir
+
+
+//URLsnr
+//$somaGS = "/usr/bin/";  // path a l'executable de Ghostscript al nou servidor www.pliegos.net de Teixidora
 $somaGS = "/usr/local/bin/";  // path a l'executable de Ghostscript al localhost del Macbook Air
 
-//$baseurlPDF = "http://www.pliegos.net/maker/pdf/";  // base url al pdf al servidor www.pliegos.net del DreamHost
-$baseurlPDF = "http://localhost/www.pliegos.net/maker/pdf/";  // base url al pdf al localhost del Macbook Air
+
+// EP! ens cal treure les www doncs el servidor de Teixidora té ara un problema al WordPress que fa que bloqui l'accés URL
+//$baseurlPDF = "http://pliegos.net/maker/plegaVeu/pdf/";  // base url al pdf al nou servidor www.pliegos.net de Teixidora
+//$baseurlPDF = "http://localhost/www.pliegos.net/maker/plegaVeu/pdf/";  // base url al pdf al localhost del Macbook Air
+
+//URLsnr
+$baseurlPDF = "http://localhost/www.pliegos.net/maker/sonarD2023/pdf/";  // base url al pdf al localhost del MacbookAir
+
+
+//$baseURL = "http://www.pliegos.net/maker";  // base url a la interfície del nou servidor www.pliegos.net de Teixidora
+//$baseURL = "http://localhost/www.pliegos.net/maker";  // base url a la interfície del localhost del Macbook Air
+
+//URLsnr
+$baseURL = "http://localhost/www.pliegos.net/maker/sonarD2023/pliegOmaker.php";  // execució recursiva al localhost del MacbookAir
 
 
 $PSapplet = $somaPS . "nUp_pliegOS.ps";
-$pdfnomes = $PDFunic . "_pliegOSnUp.pdf";
+
+
+//URLsnr
+//$pdfnomes = $PDFunic . "_plegaVeu.pdf";
+$pdfnomes = $PDFunic . "_sonarD2023_paper.pdf";
+
+
 $pdfFile = $somaPDF . $pdfnomes;
 
 // $command = "/usr/local/bin/gs -q -dBATCH -dNOPAUSE -dNOSAFER -sDEVICE=pdfwrite -sOutputFile=".$pdfFile." -c .setpdfwrite -f ".$PSapplet;  // servidor www2 de la UB
@@ -88,9 +131,14 @@ $pdfFile = $somaPDF . $pdfnomes;
 // GPL Ghostscript 9.## del DreamHost de pliegos.net + localhost del MacBokk Air
 
 // EP és fonamenal que la crida a l'executable (almenys al localhost del Macbook Air) hi hagi el path sencer!
-$command = $somaGS . "gs -q -dNOSAFER -o '" . $pdfFile . "' -sDEVICE=pdfwrite -dAutoRotatePages=/None -c .setpdfwrite -f '" . $PSapplet . "'";
+// aquesta crida encara és valida pel GS 9.27 del nou servidor de Teixidors
+//$command = $somaGS . "gs -q -dNOSAFER -o '" . $pdfFile . "' -sDEVICE=pdfwrite -dAutoRotatePages=/None -c .setpdfwrite -f '" . $PSapplet . "'";
+
+// EP crida a partir de la versió GS 9.55 al localhost del MacBookAir
+$command = $somaGS . "gs -q -dNOSAFER -o '" . $pdfFile . "' -sDEVICE=pdfwrite -dAutoRotatePages=/None -f '" . $PSapplet . "'";
 
 //echo $command;
+//exit(" <<<< EP!");
 
 // temps d'espera frisso?
 //echo("<center><img src='frisso.gif' /></center>");
@@ -121,14 +169,15 @@ ob_end_clean();
 // si torna 1 és que hi ha hagut un ofendingcommand (error a l'execució del ps)
 // si torna 0 és que el .ps s'ha executat sense errors
 
-//exit("...que ha fet?");
+// exit("...que ha fet?");
 
 // com avaluem si s'ha generat el pdf correctament?
 if ($ElQtorna == 127)
 {  // el gs NO s'ha executat
  // exit("...sembla que el GS no s'ha executat");
- echo "<center><span style='color:#ff0000;font-family:monospace;font-size:24px'><br><br>...l'int&egrave;rpret Ghostscript no s'ha executat...</span>";
- exit("<br><p><br><p><span style='color:#ff0000;font-family:monospace;font-size:24px'><a style='color:#ff0000;font-family:monospace;font-size:24px' href='mailto:marcantoni@femfum.com'>documenta'ns l'error a l'email, gr&agrave;cies!</a><br><br><a style='color:#ff0000;font-family:monospace;font-size:18px' href='http://www.pliegos.net/maker'>piegO-Maker</a></center>");
+ echo "<center><span style='color:#ff0000;font-family:monospace;font-size:24px'><br><br>&gt;&gt;&gt; l'int&egrave;rpret Ghostscript no s'ha executat &lt;&lt;&lt;</span>";
+//URLsnr
+ exit("<br><p><br><p><span style='color:#ff0000;font-family:monospace;font-size:24px'><a style='color:#ff0000;font-family:monospace;font-size:24px' href='mailto:marcantoni@femfum.com'>documenta'ns l'error via email, gr&agrave;cies!</a><br><br><a style='color:#ff0000;font-family:monospace;font-size:18px' href='$baseURL'>random sonar+PAPER</a></center>");
 
 }
 {
@@ -175,9 +224,10 @@ if ($ElQtorna == 127)
  else
  { // podem provocar errors executant sense interfície amb només comandes via URL (captura GET)
   // aquí llistem l'ERROR del prompt i demanem que s'enviï
-  echo "<center><span style='color:#ff0000;font-family:monospace;font-size:24px'><br><br>...ERROR d'execuci&oacute; de l'algorisme...</span>";
+  echo "<center><span style='color:#ff0000;font-family:monospace;font-size:24px'><br><br>&gt;&gt;&gt; ERROR d'execuci&oacute; de l'algorisme &lt;&lt;&lt;</span>";
   echo "<br><br><span style='color:#999999;font-family:monospace;font-size:24px'>".$prompt."</font><br></span>";
-  exit("<br><p><br><p><span style='color:#ff0000;font-family:monospace;font-size:24px'><a style='color:#ff0000;font-family:monospace;font-size:24px' href='mailto:marcantoni@femfum.com'>documenta'ns l'error a l'email, gr&agrave;cies!</a><br><br><a style='color:#ff0000;font-family:monospace;font-size:18px' href='http://www.pliegos.net/maker'>piegO-Maker</a></center>");
+//URLsnr
+  exit("<br><p><br><p><span style='color:#ff0000;font-family:monospace;font-size:24px'><a style='color:#ff0000;font-family:monospace;font-size:24px' href='mailto:marcantoni@femfum.com'>documenta'ns l'error via email, gr&agrave;cies!</a><br><br><a style='color:#ff0000;font-family:monospace;font-size:18px' href='$baseURL'>random sonar+PAPER</a></center>");
  }
 }
 
